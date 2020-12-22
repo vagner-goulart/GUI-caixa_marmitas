1251c834a0bf2a7be52ba621261db4e98352b5a1
vagnerPG <vagnergoulart28@gmail.com>
2020-12-22 20:40:15 -0300

    Classes que servem de base para criar certos objetos do tkintermovidas para o novo arquivo 'my_base_tkinter_objects'. Esses objetos agora podem ser importados em outros arquivos

--- caixa_marmitas_main/principal.py
+++ caixa_marmitas_main/principal.py
@@ -1,49 +1,9 @@
 # estarei usando 'sim' e 'não' para onValue e offValue das checkbox
+import sys
+sys.path.append(".")
 
 from tkinter import *
-
-class BaseCheckBox():
-
-    def __init__(self, janela):
-        self.chek_var = StringVar()
-        self.check_box = Checkbutton(
-            janela, bg=None,
-            variable=self.chek_var,
-            onvalue="sim", offvalue="não")
-
-        self.chek_var.set("nao")
-
-        self.chek_var.trace_add("write", self.printar_teste)
-
-    def printar_teste(self, *args):
-        print("slamano")
-        print(self.chek_var.get())
-        print(args)
-
-class BaseDropdownMenu():
-
-    def __init__(self, janela, lista_de_opcoes):
-        self.current_value = StringVar()
-        self.opcoes = lista_de_opcoes
-        self.dropdown_menu = OptionMenu(
-            janela, self.current_value,
-            *self.opcoes)
-        
-        self.dropdown_menu.config(width=15, font=("SourceSansPro", 13))
-
-        self.current_value.set(self.opcoes[0])
-
-
-class BaseDropdownMenuForQuant():
-
-    def __init__(self, janela):
-        self.current_value = IntVar()
-        self.quant = [1,2,3,4,5]
-        self.dropdown_menu = OptionMenu(
-            janela, self.current_value,
-            *self.quant)
-
-        self.current_value.set(self.quant[0])
+from my_base_tkinter_objects import BaseCheckBox, BaseDropdownMenu, BaseDropdownMenuForQuant
 
 
 class Foo():
