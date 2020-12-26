e48a08e9a32675aab135fedb09d556f1010b06ac
vagnerPG <vagnergoulart28@gmail.com>
2020-12-26 20:30:49 -0300

    multiple changes:
    inheritance added to all classes,
    'load_on_off_images' method created in 'BaseCheckBox'

--- caixa_marmitas_main/my_base_tkinter_objects.py
+++ caixa_marmitas_main/my_base_tkinter_objects.py
@@ -1,27 +1,32 @@
 from tkinter import *
+from PIL import ImageTk, Image
 
 
-class BaseCheckBox():
+class BaseCheckBox(Checkbutton):
 
     def __init__(self, janela):
+        super().__init__(janela)
         self.chek_var = StringVar()
-        self.check_box = Checkbutton(
-            janela, bg="green",
+        self.config(
+            bg="green",
             variable=self.chek_var,
             onvalue="sim", offvalue="nao",)
 
-        self.chek_var.set("nao")
+        self.deselect()
 
-class BaseDropdownMenu():
+    def load_on_off_images(self, on_img, off_img):
+        self.config(image=off_img, selectimage=on_img, indicatoron=False,)
+
+class BaseDropdownMenu(OptionMenu):
 
     def __init__(self, janela, lista_de_opcoes):
         self.current_value = StringVar()
         self.opcoes = lista_de_opcoes
-        self.dropdown_menu = OptionMenu(
+        super().__init__(
             janela, self.current_value,
             *self.opcoes)
         
-        self.dropdown_menu.config(
+        self.config(
             width=15,
             font=("SourceSansPro", 13),
             state=DISABLED)
@@ -29,37 +34,33 @@ class BaseDropdownMenu():
         self.current_value.set(self.opcoes[0])
 
 
-class BaseDropdownMenuForQuant():
+class BaseDropdownMenuForQuant(OptionMenu):
 
     def __init__(self, janela):
         self.current_value = IntVar()
         self.quant = [1,2,3,4,5]
-        self.dropdown_menu = OptionMenu(
+        super().__init__(
             janela, self.current_value,
             *self.quant)
 
-        self.dropdown_menu.config(state=DISABLED)
+        self.config(state=DISABLED)
 
         self.current_value.set(self.quant[0])
 
 
-class FrameDisplayValue():
+class FrameDisplayValue(Frame):
 
     def __init__(self, janela, valor=0.0):
-
-        self.frame = Frame(janela, bg=None)
+        super().__init__(janela, bg=None)
 
         generic_txt_label = Label(
-            self.frame, text="VALOR:",
-            font=("SourceSansPro", 11))
+            self, text="VALOR:", font=("SourceSansPro", 11))
         generic_txt_label.grid(row=0, column=0, stick=E)
 
         label_valor_3 = Label(
-            self.frame,
-            text=valor,
-            font=(None, 11),
+            self,
+            text=valor, font=(None, 11),
+            borderwidth=1, relief='solid',
             width=8,
-            borderwidth=1,
-            relief='solid',
             anchor="e")
         label_valor_3.grid(row=0, column=1, padx=(1,1), stick=W)
