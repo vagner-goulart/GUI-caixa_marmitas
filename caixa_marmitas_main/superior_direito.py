83baa77c48283cc78dca281313e47644f84aae61
vagnerPG <vagnergoulart28@gmail.com>
2020-12-28 18:06:39 -0300

    New:
    The value '+900+100' was added to the geometry method so that the tkinter window apear on the right side of the screen, making it better to develop the code,
    the values in 'frame_marmitas' will not stay and were added just to make developing the code better/easier,
    instead of using '.pack()' and '.pack_propagate()' i'm using 'grid()' so that 'frame_marmitas' frame doesn't change it's size.

--- caixa_marmitas_main/superior_direito.py
+++ caixa_marmitas_main/superior_direito.py
@@ -41,5 +41,20 @@ class Barr():
         else:
             self.menu_quant.config(state=DISABLED)
 
+root_window = Tk()
+root_window.geometry("400x400+900+100")
 
+frame_marmitas = Frame(root_window, width=250, height=250, bg="#E0E0E0")
+
+marm_pequena = Barr(frame_marmitas, 0, "PEQUENA")
+marm_media = Barr(frame_marmitas, 1, "MEDIA")
+marm_grande = Barr(frame_marmitas, 2, "GRANDE")
+
+
+frame_valor = FrameDisplayValue(frame_marmitas)
+frame_valor.grid(stick=S, pady=(15,0))
+
+frame_marmitas.grid()
+frame_marmitas.grid_propagate(False)
+root_window.mainloop()
 
