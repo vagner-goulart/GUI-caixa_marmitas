0e33171e8ec4e5dcc05ef5a99150ebb89f8671e7
vagnerPG <vagnergoulart28@gmail.com>
2020-12-28 18:41:40 -0300

    Created frame ('frame_inf_esq') to hold both title and prato_dia labels, and added 'label_prato_dia' to it.

--- caixa_marmitas_main/infeiror_esquerdo.py
+++ caixa_marmitas_main/infeiror_esquerdo.py
@@ -9,3 +9,16 @@ from my_base_tkinter_objects import (
     BaseDropdownMenuForQuant, FrameDisplayValue)
 
 
+root_window = Tk()
+root_window.geometry("400x400+900+100")
+
+frame_inf_esq = Frame(root_window ,width=200, height=200 ,bg="red", bd=2)
+
+prato_dia = "Arroz, Feijão, Macarrão ao molho vermelho,\nFarofa, Bisteca de porco, Salsicha ao molho.\nFrio: Alface e Tomate."
+
+label_prato_dia = Label(frame_inf_esq, text=prato_dia)
+label_prato_dia.grid()
+
+frame_inf_esq.grid()
+#frame_inf_esq.grid_propagate(False)
+root_window.mainloop()
