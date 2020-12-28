741ba2f66cbd6bc33185c39f3b7039e87196f396
vagnerPG <vagnergoulart28@gmail.com>
2020-12-28 18:21:26 -0300

    sys, tkinter and pillow imports remaing the same and were just copied from 'superior_esquerdo' file

---
+++ caixa_marmitas_main/infeiror_esquerdo.py
@@ -0,0 +1,11 @@
+import sys
+sys.path.append(".")
+
+from tkinter import *
+from PIL import ImageTk, Image
+
+from my_base_tkinter_objects import (
+    BaseCheckBox, BaseDropdownMenu,
+    BaseDropdownMenuForQuant, FrameDisplayValue)
+
+
