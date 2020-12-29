495a4193e4fc0845de97059c3c0a2f405f23fc51
vagnerPG <vagnergoulart28@gmail.com>
2020-12-29 14:25:25 -0300

    added base imports

---
+++ caixa_marmitas_main/infeiror_direito.py
@@ -0,0 +1,9 @@
+import sys
+sys.path.append(".")
+
+from tkinter import *
+from PIL import ImageTk, Image
+
+from my_base_tkinter_objects import (
+    BaseCheckBox, BaseDropdownMenu,
+    BaseDropdownMenuForQuant, FrameDisplayValue)
