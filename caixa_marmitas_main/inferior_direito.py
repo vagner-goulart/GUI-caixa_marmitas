8d31144960ce505d355cbc8f948210e9784c7e84
vagnerPG <vagnergoulart28@gmail.com>
2020-12-30 17:25:03 -0300

    Class 'BuildBarrObject' now inherit from frame, so now 'BuildBarrObject' it's a frame itself. Also made changes to match the inheritance

--- caixa_marmitas_main/inferior_direito.py
+++ caixa_marmitas_main/inferior_direito.py
@@ -9,19 +9,18 @@ from my_base_tkinter_objects import (
     BaseDropdownMenuForQuant, FrameDisplayValue)
 
 
-class BuildBarObject():
+class BuildBarObject(Frame):
 
-    def __init__(self, janela, texto):
+    def __init__(self, janela, texto, **kwargs):
+        super().__init__(janela, **kwargs)
 
-        this_frame = Frame(janela)
-
-        generic_txt_label = Label(this_frame, text=texto+":", width=5, anchor=W, font=("SourceSansPro", 20))
+        generic_txt_label = Label(self, text=texto+":", width=5, anchor=W, font=("SourceSansPro", 20))
         generic_txt_label.grid(row=0, column=0, sticky=W)
         
-        value_label = Label(this_frame, text="0.00", bd=2, relief='solid', width=10, anchor=E, font=("SourceSansPro", 20))
+        value_label = Label(self, text="0.00", bd=2, relief='solid', width=10, anchor=E, font=("SourceSansPro", 20))
         value_label.grid(row=0, column=1, sticky=W)
 
-        this_frame.grid(sticky=W, padx=(5,0), pady=(10,0))
+        self.grid(sticky=W, padx=(5,0), pady=(10,0))
 
 
 root_window = Tk()
@@ -38,6 +37,6 @@ check_sla.grid(sticky=W, padx=(5,0), pady=(10,0))
 test_1 = BuildBarObject(sla_frame, "Input")
 test_2 = BuildBarObject(sla_frame, "Troco")
 
-# sla_frame.grid()
-# #sla_frame.grid_propagate(False)
-# root_window.mainloop()
+sla_frame.grid()
+#sla_frame.grid_propagate(False)
+root_window.mainloop()
