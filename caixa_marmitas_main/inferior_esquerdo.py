66251ed2cf0bd9b993052d60c0b49400a5cc9545
vagnerPG <vagnergoulart28@gmail.com>
2020-12-30 19:05:36 -0300

    'PratoDiaFrame' class created to create the bottom left corner frame

--- caixa_marmitas_main/inferior_esquerdo.py
+++ caixa_marmitas_main/inferior_esquerdo.py
@@ -9,18 +9,31 @@ from my_base_tkinter_objects import (
     BaseDropdownMenuForQuant, FrameDisplayValue)
 
 
+class PratoDiaFrame(Frame):
+
+    def __init__(self, janela, dia, ingredientes, **kwargs):
+        super().__init__(janela, **kwargs)
+
+        self.titulo_dia_semana = "PRATO DO DIA - {}".format(dia)
+
+        self.label_titulo_dia_semana = Label(
+            janela, text=self.titulo_dia_semana, bg='#E0E0E0', font=("SourceSansPro", 12),)
+        self.label_titulo_dia_semana.pack(pady=(10,5))
+        
+        self.label_prato_dia = Label(janela, text=ingredientes, bg='#E0E0E0', font=("SourceSansPro", 11),)
+        self.label_prato_dia.pack()
+
+
 root_window = Tk()
 root_window.geometry("400x400+900+100")
 
 frame_inf_esq = Frame(root_window ,width=200, height=200 ,bg="#E0E0E0", bd=2)
 
-titulo_dia_semana = "PRATO DO DIA - {}".format("SEGUNDA")
-label_titulo_dia_semana = Label(frame_inf_esq, text=titulo_dia_semana, bg='#E0E0E0', font=("SourceSansPro", 12),)
-label_titulo_dia_semana.pack(pady=(10,5))
+dia_semana = "SEGUNDA"
 
-prato_dia = "Arroz, Feijão, Macarrão ao molho vermelho,\nFarofa, Bisteca de porco, Salsicha ao molho.\nFrio: Alface e Tomate."
-label_prato_dia = Label(frame_inf_esq, text=prato_dia, bg='#E0E0E0', font=("SourceSansPro", 11),)
-label_prato_dia.pack()
+ingredientes_dia = "Arroz, Feijão, Macarrão ao molho vermelho,\nFarofa, Bisteca de porco, Salsicha ao molho.\nFrio: Alface e Tomate."
+
+prato_dia_frame = PratoDiaFrame(frame_inf_esq, dia_semana, ingredientes_dia)
 
 frame_inf_esq.grid()
 #frame_inf_esq.grid_propagate(False)
