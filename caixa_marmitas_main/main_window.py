47839c32386341af45148944f68703f2c5bbd291
vagnerPG <vagnergoulart28@gmail.com>
2021-01-02 20:22:22 -0300

    added import to get the class responsible for buiding the objects to the bottom left corner (parte do prato do dia)

--- caixa_marmitas_main/main_window.py
+++ caixa_marmitas_main/main_window.py
@@ -5,7 +5,7 @@ from tkinter import *
 from PIL import ImageTk, Image
 
 from my_base_tkinter_objects import FrameDisplayValue
-from app_frames import BebidasFrames, MarmitasFrames
+from app_frames import BebidasFrames, MarmitasFrames, PratoDiaFrame
 
 root_window = Tk()
 root_window.geometry('600x600+600+50')
