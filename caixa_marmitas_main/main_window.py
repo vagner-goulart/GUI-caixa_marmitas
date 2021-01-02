d05a797c9727fa846c7cca075e30efcd50b40944
vagnerPG <vagnergoulart28@gmail.com>
2021-01-02 19:21:44 -0300

    Added imports to get the classes to build the top left corner (parte das bebidas

--- caixa_marmitas_main/main_window.py
+++ caixa_marmitas_main/main_window.py
@@ -4,6 +4,8 @@ sys.path.append(".")
 from tkinter import *
 from PIL import ImageTk, Image
 
+from my_base_tkinter_objects import FrameDisplayValue
+from app_frames import BebidasFrames
 
 root_window = Tk()
 root_window.geometry('600x600+600+50')
