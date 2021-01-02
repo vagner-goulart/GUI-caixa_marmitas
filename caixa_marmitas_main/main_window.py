0d8175963c356d05ef827ca5e9f75519b084df4e
vagnerPG <vagnergoulart28@gmail.com>
2021-01-02 19:44:27 -0300

    Created the objects that hold the on and off images

--- caixa_marmitas_main/main_window.py
+++ caixa_marmitas_main/main_window.py
@@ -10,6 +10,11 @@ from app_frames import BebidasFrames
 root_window = Tk()
 root_window.geometry('600x600+600+50')
 
+# the img being open here is in the same dir as the principal.py file,
+# the problem is with vscode that doesn't recognize that. idkw
+on_image = ImageTk.PhotoImage(Image.open("caixa_marmitas_main/13x13_on_img.png"))
+off_image = ImageTk.PhotoImage(Image.open("caixa_marmitas_main/13x13_off_img.png"))
+
 
 
 root_window.mainloop()
