f71e06a3037606809f2c7911133681fc807385e0
vagnerPG <vagnergoulart28@gmail.com>
2021-01-02 14:41:03 -0300

    Class 'BuildBarObjects' renamed to 'TrocoFrames', self added to objects inside 'TrocoFrames' so that they can be accessed as attribeutes

--- caixa_marmitas_main/inferior_direito.py
+++ caixa_marmitas_main/inferior_direito.py
@@ -9,16 +9,17 @@ from my_base_tkinter_objects import (
     BaseDropdownMenuForQuant, FrameDisplayValue)
 
 
-class BuildBarObject(Frame):
+class TrocoFrames(Frame):
 
     def __init__(self, janela, texto, **kwargs):
         super().__init__(janela, **kwargs)
 
-        generic_txt_label = Label(self, text=texto+":", width=5, anchor=W, font=("SourceSansPro", 20))
-        generic_txt_label.grid(row=0, column=0, sticky=W)
+        self.generic_txt_label = Label(self, text=texto+":", width=5, anchor=W, font=("SourceSansPro", 20))
+        self.generic_txt_label.grid(row=0, column=0, sticky=W)
         
-        value_label = Label(self, text="0.00", bd=2, relief='solid', width=10, anchor=E, font=("SourceSansPro", 20))
-        value_label.grid(row=0, column=1, sticky=W)
+        self.value_label = Label(
+            self, text="0.00", bd=2, relief='solid', width=10, anchor=E, font=("SourceSansPro", 20))
+        self.value_label.grid(row=0, column=1, sticky=W)
 
         self.grid(sticky=W, padx=(5,0), pady=(10,0))
 
@@ -28,14 +29,14 @@ root_window.geometry("400x400+900+100")
 
 sla_frame = Frame(root_window, width=300, height=300, bg='red')
 
-test = BuildBarObject(sla_frame, "Total")
+test = TrocoFrames(sla_frame, "Total")
 
 check_sla = BaseCheckBox(sla_frame)
 check_sla.config(text="Pagar com cartão", font=("SourceSansPro", 15))
 check_sla.grid(sticky=W, padx=(5,0), pady=(10,0))
 
-test_1 = BuildBarObject(sla_frame, "Input")
-test_2 = BuildBarObject(sla_frame, "Troco")
+test_1 = TrocoFrames(sla_frame, "Input")
+test_2 = TrocoFrames(sla_frame, "Troco")
 
 sla_frame.grid()
 #sla_frame.grid_propagate(False)
