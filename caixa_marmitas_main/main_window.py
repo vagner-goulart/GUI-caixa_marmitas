60e21760351a250b614a5fd42ddb20ee8ff5cddf
vagnerPG <vagnergoulart28@gmail.com>
2021-01-02 20:42:14 -0300

    Added 'BaseCheckBox' class to be imported because the bottom right corner (parte do pagamento) will use it

--- caixa_marmitas_main/main_window.py
+++ caixa_marmitas_main/main_window.py
@@ -4,8 +4,7 @@ sys.path.append(".")
 from tkinter import *
 from PIL import ImageTk, Image
 
-from my_base_tkinter_objects import FrameDisplayValue
-from app_frames import BebidasFrames, MarmitasFrames, PratoDiaFrame
+from my_base_tkinter_objects import FrameDisplayValue, BaseCheckBox
 
 root_window = Tk()
 root_window.geometry('600x600+600+50')
