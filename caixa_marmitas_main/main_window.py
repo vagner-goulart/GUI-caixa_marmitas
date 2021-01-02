78f74c11cc606358c6b5eacad486951ec40d87e7
vagnerPG <vagnergoulart28@gmail.com>
2021-01-02 20:17:26 -0300

    Creating the top right corner frame and packing it into the screen

--- caixa_marmitas_main/main_window.py
+++ caixa_marmitas_main/main_window.py
@@ -34,7 +34,22 @@ frame_outros.check_caixa.load_on_off_images(on_image, off_image)
 frame_valor = FrameDisplayValue(frame_bebidas_outros, "0.00")
 frame_valor.grid(column=1, pady=(10,0))
 
+
+# top right corner (parte das marmitas)
+frame_marmitas = Frame(root_window, width=250, height=250, bg="#E0E0E0")
+
+marm_pequena = MarmitasFrames(frame_marmitas, 0, "PEQUENA")
+marm_media = MarmitasFrames(frame_marmitas, 1, "MEDIA")
+marm_grande = MarmitasFrames(frame_marmitas, 2, "GRANDE")
+
+frame_valor = FrameDisplayValue(frame_marmitas)
+frame_valor.grid(pady=(15,0))
+
+frame_marmitas.grid(row=0, column=1)
+
+
 # packing the frame with the objects on the screen
+
 # top left (bebidas)
 frame_bebidas_outros.grid(row=0, column=0)
 
