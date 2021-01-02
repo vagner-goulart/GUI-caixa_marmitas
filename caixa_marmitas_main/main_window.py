29a5054219cb95f0b4ed984b41a276519c7e1634
vagnerPG <vagnergoulart28@gmail.com>
2021-01-02 19:45:41 -0300

    Creating the top left corner frame and packing it into the screen

--- caixa_marmitas_main/main_window.py
+++ caixa_marmitas_main/main_window.py
@@ -16,5 +16,25 @@ on_image = ImageTk.PhotoImage(Image.open("caixa_marmitas_main/13x13_on_img.png")
 off_image = ImageTk.PhotoImage(Image.open("caixa_marmitas_main/13x13_off_img.png"))
 
 
+# creating the top left corner (parte das bebidas)
+
+frame_bebidas_outros = Frame(root_window, width=300, height=30)
+frame_bebidas_outros.config(bg="blue")
+
+lista_bebidas = ["Água", "CocaCola", "Pepsi"]
+frame_bebidas = BebidasFrames(frame_bebidas_outros, 0, (10,20), lista_bebidas)
+
+lista_outros = ["Halls", "Trident", "Bala"]
+frame_outros = BebidasFrames(frame_bebidas_outros, 1, (20, 10), lista_outros)
+
+frame_bebidas.check_caixa.load_on_off_images(on_image, off_image)
+frame_outros.check_caixa.load_on_off_images(on_image, off_image)
+
+frame_valor = FrameDisplayValue(frame_bebidas_outros, "0.00")
+frame_valor.grid(column=1, pady=(10,0))
+
+# packing the frame with the objects on the screen
+frame_bebidas_outros.pack()
+frame_bebidas_outros.pack_propagate(False)
 
 root_window.mainloop()
