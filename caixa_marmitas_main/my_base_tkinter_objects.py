b7871dbd028648a2b154fea3c3825859e43d10f4
vagnerPG <vagnergoulart28@gmail.com>
2021-01-04 15:11:09 -0300

    Added tkinter variable to hold the value

--- caixa_marmitas_main/my_base_tkinter_objects.py
+++ caixa_marmitas_main/my_base_tkinter_objects.py
@@ -52,6 +52,8 @@ class FrameDisplayValue(Frame):
     def __init__(self, janela, valor=0.00):
         super().__init__(janela, bg=None)
 
+        self.value_var = DoubleVar(self, value=valor)
+
         generic_txt_label = Label(
             self, text="VALOR:", font=("SourceSansPro", 11), bg="#E0E0E0",)
         generic_txt_label.grid(row=0, column=0, stick=E)
