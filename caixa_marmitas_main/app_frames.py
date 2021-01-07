2a61a5fb473c98e6eb1026d85feb5262f0bb2449
vagnerPG <vagnergoulart28@gmail.com>
2021-01-07 16:26:54 -0300

    GIT isn't working as expected in this file (app_frames.py), so i will commit all changes at once even though they don't apear here. The changes are:
    Changed the atttibute 'check_caixa' to 'check_box' so the file stay consistent,
    created the method 'get_quant_menu_var_name' which gets the name of the variable bounded to the quantity menu,
    created 'CommonMethodsBebidasMarmitas' class to be inherited by 'BebidasFrames' and 'MarmitaFrames',
    moved the methods from 'BebidasFrames' to 'CommonMethodsBebidasMarmitas' and now with that, 'BebidasFrames' and 'MarmitasFrames' have the same methods as it

--- caixa_marmitas_main/app_frames.py
+++ caixa_marmitas_main/app_frames.py
@@ -7,45 +7,20 @@ from my_base_tkinter_objects import (
     BaseCheckBox, BaseDropdownMenu,
     BaseDropdownMenuForQuant, FrameDisplayValue)
 
-class BebidasFrames(Frame):
+class CommonMethodsBebidasMarmitas:
+    """Internal class.
+    
+    This class is only for 'BebidasFrames' and 'MarmitasFrames' inheritance only,
+    as it only has methods used by them both."""
 
-    def __init__(self, janela, linha, pading, lista_d_items, var_nome, **kwargs):
-        super().__init__(janela,bg='red', **kwargs)        
-        
-        self.check_caixa = BaseCheckBox(self)
-        self.check_caixa.grid(row=linha, column=0)
-
-        self.menu_de_items = BaseDropdownMenu(self, lista_d_items)
-        self.menu_de_items.grid(row=linha, column=1, padx=(25,20))
-
-        self.menu_de_quantidade = BaseDropdownMenuForQuant(self, var_nome)
-        self.menu_de_quantidade.grid(row=linha, column=2)
-
-        self.grid(pady=pading, columnspan=3)
-
-        self.check_caixa.chek_var.trace_add('write', self.grey_out_elements)
-
-        self.added_value = 0
-        self.previous_quant = self.get_quant_from_quantmenu()
-
-    # TODO: esse metodo, assim como o trace_add acima, ta estranho. arruma dps
-    def grey_out_elements(self, *args):
-        check_buton_state = self.check_caixa.chek_var.get()
-        self.update_value_label()
-
-        if check_buton_state == "sim":
-            self.menu_de_items.config(state=NORMAL)
-            self.menu_de_quantidade.config(state=NORMAL)
-        
-        else:
-            self.menu_de_items.config(state=DISABLED)
-            self.menu_de_quantidade.config(state=DISABLED)
+    def get_quant_menu_var_name(self):
+        return str(self.menu_de_quantidade.current_value) # pylint: disable=no-member
 
     def get_quant_from_quantmenu(self):
-        return self.menu_de_quantidade.current_value.get()
+        return self.menu_de_quantidade.current_value.get() # pylint: disable=no-member
 
     def get_checkbox_state(self):
-        return self.check_caixa.chek_var.get()
+        return self.check_box.chek_var.get() # pylint: disable=no-member
 
     @property
     def checkbox_is_active(self):
@@ -56,9 +31,7 @@ class BebidasFrames(Frame):
         self.value_var_to_update = var.value_var
 
     def add_traces_to_dropdownmenus(self):
-        # adding a trace to the items menu is not necessary in the moment
-        #self.menu_de_items.current_value.trace_add('write', self.update_value_label)
-        self.menu_de_quantidade.current_value.trace_add('write', self.update_value_label)
+        self.menu_de_quantidade.current_value.trace_add('write', self.update_value_label) # pylint: disable=no-member
  
     def update_value_label(self, v_name=None, *args):
         new_value = self.calculate_new_value(v_name)
@@ -68,24 +41,60 @@ class BebidasFrames(Frame):
         current_val = self.value_var_to_update.get()
         quant = self.get_quant_from_quantmenu()
 
-        print(f"c_val= {current_val}\n", f"added_value= {self.added_value}")
+        print(f"c_val= {current_val}\n", f"added_value= {self.added_value}") # pylint: disable=access-member-before-definition
 
         if not self.checkbox_is_active and current_val > 0:
             self.added_value = -(quant*5.0)
         else:
             self.added_value = quant*5.0
 
-        if v_name in ["bebidas", "outros"]:
-            self.added_value -= self.previous_quant*5.0
+        if v_name == self.get_quant_menu_var_name():
+            self.added_value -= self.previous_quant*5.0 # pylint: disable=access-member-before-definition
 
         self.previous_quant = self.get_quant_from_quantmenu()
         print(f"c_val= {current_val}\n", f"added_value= {self.added_value}")
         print(v_name)
+        print(self.get_quant_menu_var_name())
 
         return self.added_value + current_val
-        
 
-class MarmitasFrames(Frame):
+
+class BebidasFrames(Frame, CommonMethodsBebidasMarmitas):
+
+    def __init__(self, janela, linha, pading, lista_d_items, var_nome, **kwargs):
+        super().__init__(janela,bg='red', **kwargs)        
+        
+        self.check_box = BaseCheckBox(self)
+        self.check_box.grid(row=linha, column=0)
+
+        self.menu_de_items = BaseDropdownMenu(self, lista_d_items)
+        self.menu_de_items.grid(row=linha, column=1, padx=(25,20))
+
+        self.menu_de_quantidade = BaseDropdownMenuForQuant(self, var_nome)
+        self.menu_de_quantidade.grid(row=linha, column=2)
+
+        self.grid(pady=pading, columnspan=3)
+
+        self.check_box.chek_var.trace_add('write', self.grey_out_elements)
+
+        self.added_value = 0
+        self.previous_quant = self.get_quant_from_quantmenu()
+
+    # TODO: esse metodo, assim como o trace_add acima, ta estranho. arruma dps
+    def grey_out_elements(self, *args):
+        check_buton_state = self.check_box.chek_var.get()
+        self.update_value_label()
+
+        if check_buton_state == "sim":
+            self.menu_de_items.config(state=NORMAL)
+            self.menu_de_quantidade.config(state=NORMAL)
+        
+        else:
+            self.menu_de_items.config(state=DISABLED)
+            self.menu_de_quantidade.config(state=DISABLED)
+
+
+class MarmitasFrames(Frame, CommonMethodsBebidasMarmitas):
 
     def __init__(self, janela, linha, nome_marmita, var_nome, **kwargs):
         super().__init__(janela, **kwargs)
@@ -107,15 +116,21 @@ class MarmitasFrames(Frame):
 
         self.check_box.chek_var.trace_add('write', self.grey_out_elements)
 
+        self.added_value = 0
+        self.previous_quant = self.get_quant_from_quantmenu()
+
     # TODO: esse metodo, assim como o trace_add acima, ta estranho. arruma dps
     def grey_out_elements(self, *args):
         check_buton_state = self.check_box.chek_var.get()
+
+        self.update_value_label()
 
         if check_buton_state == "sim":
             self.menu_de_quantidade.config(state=NORMAL)
         
         else:
             self.menu_de_quantidade.config(state=DISABLED)
+
 
 
 class PratoDiaFrame(Frame):
