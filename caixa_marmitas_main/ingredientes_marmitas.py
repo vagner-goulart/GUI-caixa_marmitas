57e939eb978a58f979d9182c1bbfc38d9717f2e3
vagnerPG <vagnergoulart28@gmail.com>
2021-01-25 21:57:30 -0300

    Created var to hold the ingredients for each day of the week.

--- caixa_marmitas_main/ingredientes_marmitas.py
+++ caixa_marmitas_main/ingredientes_marmitas.py
@@ -1,1 +1,40 @@
 import date_and_time as dt
+
+segunda = (
+    "Arroz, Feijão, Farofa, Bisteca de Porco,\n"+
+    "Carne moída com batata, Calabresa acebolada.\n"+
+    "Refogado de repolho com cenoura.\n"+
+    "Frio: Alface e Tomate."
+    )
+
+terca = (
+    "Arroz, Feijão, Lasanha,\n"+
+    "Linguiça, Frango frito.\n"+
+    "Frio: Repolho."
+    )
+
+quarta = (
+    "Arroz, Feijão, Creme de Milho,\n"+
+    "Filé de frango, Farofa, Nhoque.\n"+
+    "Frio: Alface, Tomate.")
+
+# this one may be too long. TODO: check later
+quinta = (
+    "Arroz, Feijão, Refogado, Carne de Panela c/ Batata,\n"+
+    "Calabresa Acebolada, Linguiça assada no forno.\n"+
+    "Frio: Alface e Tomate."
+    )
+
+sexta = (
+    "Arroz, Feijão, Macarrão ao Molho Vermelho,\n"+
+    "Escondidinho de Carne, Frango ao molho.\n"+
+    "Frio: Beterraba."
+    )
+
+sabado = (
+    "Arroz, Feijão, Feijoada, Bife, Couve,\n"+
+    "Farofa, Torresmo.\n"+
+    "Acrescimo de R$ 2,00 P/\n"+
+    "marmita com feijoada")
+
+domingo = "Nenhum."
