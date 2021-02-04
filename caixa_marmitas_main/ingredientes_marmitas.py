89fe73bf86f71295427dc1d45f9d13087e563232
vagnerPG <vagnergoulart28@gmail.com>
2021-02-03 21:10:31 -0300

    Made changes so that the text adapt to the new screen and font size

--- caixa_marmitas_main/ingredientes_marmitas.py
+++ caixa_marmitas_main/ingredientes_marmitas.py
@@ -1,8 +1,10 @@
 import date_and_time as dt
 
 segunda = (
-    "Arroz, Feijão, Farofa, Bisteca de Porco,\n"+
-    "Carne moída com batata, Calabresa acebolada.\n"+
+    "Arroz, Feijão, Farofa,\n"+
+    "Bisteca de Porco,\n"+
+    "Carne moída com batata,\n"+
+    "Calabresa acebolada.\n"+
     "Refogado de repolho com cenoura.\n"+
     "Frio: Alface e Tomate."
     )
@@ -18,22 +20,25 @@ quarta = (
     "Filé de frango, Farofa, Nhoque.\n"+
     "Frio: Alface, Tomate.")
 
-# this one may be too long. TODO: check later
 quinta = (
-    "Arroz, Feijão, Refogado, Carne de Panela c/ Batata,\n"+
-    "Calabresa Acebolada, Linguiça assada no forno.\n"+
+    "Arroz, Feijão, Refogado,\n"+
+    "Carne de Panela c/ Batata,\n"+
+    "Calabresa Acebolada,\n"+
+    "Linguiça assada no forno.\n"+
     "Frio: Alface e Tomate."
     )
 
 sexta = (
-    "Arroz, Feijão, Macarrão ao Molho Vermelho,\n"+
-    "Escondidinho de Carne, Frango ao molho.\n"+
+    "Arroz, Feijão, Frango ao molho,\n"+
+    "Macarrão ao Molho Vermelho,\n"+
+    "Escondidinho de Carne.\n"+
     "Frio: Beterraba."
     )
 
 sabado = (
     "Arroz, Feijão, Feijoada, Bife, Couve,\n"+
     "Farofa, Torresmo.\n"+
+    "----------\n"+
     "Acrescimo de R$ 2,00 P/\n"+
     "marmita com feijoada")
 
