b73d7d34f844fd5234807e016e833ff3471593f1
vagnerPG <vagnergoulart28@gmail.com>
2021-02-03 21:20:15 -0300

    Removed bg color since i'm using the default tkinter window color

--- caixa_marmitas_main/my_base_tkinter_objects.py
+++ caixa_marmitas_main/my_base_tkinter_objects.py
@@ -69,7 +69,7 @@ class FrameDisplayValue(Frame):
         self.value_var = DoubleVar(self, value=valor)
 
         generic_txt_label = Label(
-            self, text="VALOR:", font=("SourceSansPro", 11), bg="#E0E0E0",)
+            self, text="VALOR:", font=("SourceSansPro", 11))
         generic_txt_label.grid(row=0, column=0, stick=E)
 
         self.label_valor_3 = Label(
